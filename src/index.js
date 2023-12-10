08c0c72fb9e0b13420a1201b6d023e2c3c3cd3f4
Patrik Mikael <patrik.m.maki@gmail.com>
2023-12-10 19:38:07 +0200

    now it works, the just rest of the stuff

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3');
 const app = express();
 app.use(express.json());
 const port = 3000;
-let cache = {};
+//let cache = {};
 let db = createDbConnection();
 
 let i = 0;
@@ -28,30 +28,35 @@ app.post('/certs', async function (req, res) {
         res.status(400).json({ "error": validation });
         return;
     }
-    const id = i++;
+    const id = makeId(8);
     const { certificate, privateKey } = createCert(id, CN, O, C, days);
-    cache[id] = { id: id, name: `/CN=${CN}, /O=${O}, /C=${C}`, days: days, certificate: certificate, privateKey: privateKey };
+    //cache[id] = { id: id, name: `/CN=${CN}, /O=${O}, /C=${C}`, days: days, certificate: certificate, privateKey: privateKey };
+    await insertCertificate(id, "name", days, certificate, privateKey);
     res.status(201).json({ "success": "Certificate created" });
 });
 
 app.get('/certs', async function (_req, res) {
-    console.log(cache);
-    res.status(200).json(cache)
+    //console.log(cache);
+    const certs = await getCertificates();
+    res.status(200).json(certs)
 });
 
 app.get('/certs/:id', async function (req, res) {
     console.log(req.params.id);
-    res.status(200).json(cache[req.params.id])
+    const certificate = await getCertificate(req.params.id);
+    console.log(certificate);
+    res.status(200).json(certificate);
 });
 
 app.delete('/certs/:id', async function (req, res) {
     const id = req.params.id;
     console.log(id)
-    console.log(cache)
-    const deleted = id in cache;
-    delete cache[id];
+    //console.log(cache)
+    //const deleted = id in cache;
+    //delete cache[id];
+    const deleted = await deleteCertificate(id);
     //TODO: check
-    console.log(cache)
+    //console.log(cache)
     if (deleted) {
         res.status(200).json({ status: "ok" });
     } else {
@@ -63,17 +68,26 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-//similar to the validate on the frontend
+//helpers
 function validate(CN, O, C, days) {
     if (CN === "") return "Common name cannot be empty";
     if (C === "") return "A country is needed";
     if (days === "") return "validity in days must be given";
     return "";
 }
+function makeId(length) {
+    var result           = '';
+    var characters       = '0123456789ABCDEF';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 
 //database helpers:
 function createDbConnection() {
-    const db = new sqlite3.Database(filepath, (error) => {
+    const db = new sqlite3.Database("certs.db", (error) => {
         if (error) {
             return console.error(error.message);
         }
@@ -85,11 +99,11 @@ function createDbConnection() {
 
 function createTable(db) {
     db.exec(`
-    CREATE TABLE certs
+    CREATE TABLE IF NOT EXISTS certs
     (
-      ID INTEGER NOT NULL,
-      name   TEXT NOT NULL,
-      days   INTEGER NOT NULL,
+      ID TEXT NOT NULL,
+      name TEXT NOT NULL,
+      days INTEGER NOT NULL,
       certificate TEXT NOT NULL,
       privateKey TEXT NOT NULL
     );
@@ -119,7 +133,7 @@ function getCertificates() {
             } else {
                 const result = []
                 rows.forEach((row) => {
-                    console.log(row.name);
+                    console.log(row.id);
                     result.push(row);
                 });
                 resolve(result);
@@ -130,17 +144,13 @@ function getCertificates() {
 
 function getCertificate(id) {
     return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM certs WHERE id=?';
-        db.all(sql, [id], function (err, rows) {
+        const sql = 'SELECT * FROM certs WHERE id = ?';
+        db.get(sql, [id], function (err, row) {
             if (err) {
                 reject(err);
             } else {
-                const result = []
-                rows.forEach((row) => {
-                    console.log(row.name);
-                    result.push(row);
-                });
-                resolve(result);
+                console.log(row.id);
+                resolve(row);
             }
         });
     });
@@ -148,13 +158,13 @@ function getCertificate(id) {
 
 function deleteCertificate(id) {
     return new Promise((resolve, reject) => {
-        const sql = 'DELETE FROM users WHERE id=?';
+        const sql = 'DELETE FROM certs WHERE id = ?';
         db.run(sql, [id], function (err) {
             if (err) {
                 reject(err);
             } else {
                 console.log(`Row deleted with ID: ${id}`);
-                resolve();
+                resolve(true);
             }
         });
     });
@@ -179,7 +189,7 @@ function createCert(id, CN, O, C, days) {
     const cert = forge.pki.createCertificate();
     cert.publicKey = publicKey;
     cert.privateKey = privateKey;
-    cert.serialNumber = id.toString(16);
+    cert.serialNumber = id;
     cert.validity.notBefore = getCertNotBefore();
     cert.validity.notAfter = getCertNotAfter(days);
     cert.setSubject(attributes);
